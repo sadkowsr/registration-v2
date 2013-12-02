89e2aceae0f55f45eeb8ce8cc7c050f17558f5e5
MZielinski <maciej.zielinski@rulefinancial.com>
2013-12-02 11:48:12 +0100

    remove comment from AppConfig

--- mzielinski/src/main/java/com/mzielinski/gitkata/config/AppConfig.java
+++ mzielinski/src/main/java/com/mzielinski/gitkata/config/AppConfig.java
@@ -3,9 +3,6 @@ package com.mzielinski.gitkata.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * @author mzielinski, Rule Financial
- */
 @Configuration
 @ComponentScan("com.mzielinski.gitkata")
 public class AppConfig {
